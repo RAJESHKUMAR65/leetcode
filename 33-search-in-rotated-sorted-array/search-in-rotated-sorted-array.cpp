cc5a2ecbfd5908b49081500a9ba1c36a38861e68
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2023-09-04 01:35:00 +0530

    Time: 3 ms (67.71%) | Memory: 11.3 MB (19.72%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int binary_search(vector<int> arr,int target,int start , int end)
+{
+    
+    int mid=start + (end-start)/2;
+    while(start<=end)
+    { int element=arr[mid]; 
+       if(element==target)
+        {
+            return mid;
+             
+        }
+        else if(target<element)
+        {
+           end=mid-1;
+            
+        }
+        else{
+            start=mid+1;
+             
+        }
+        mid = start+(end-start)/2;
+    }
+    return -1;
+}
+ int findpivot(vector<int> arr)
+{
+    int s=0;
+    int e=arr.size()-1;
+    int mid=s+(e-s)/2;
+    while(s<e)
+    {
+        if(mid+1<arr.size() && arr[mid]>arr[mid+1])
+        {
+            return mid;
+
+        }
+        if(mid-1>0 && arr[mid-1]>arr[mid])
+        {
+            return mid-1;
+        }
+        if(arr[s]>=arr[mid])
+        { 
+           e=mid-1;
+
+        }
+         if(arr[s]<arr[mid])
+        { 
+           s=mid;
+
+        }
+        mid=s+(e-s)/2;
+    }
+    return s;
+}
+    int search(vector<int>& nums, int target) {
+        int pivotindex=findpivot(nums);
+        if(target>=nums[0]&&target<=nums[pivotindex])
+        {
+            int ans=binary_search(nums,target,0,pivotindex);
+            return ans;
+        }
+        if(pivotindex+1<nums.size()&&target>=nums[pivotindex+1]&&target<=nums[nums.size()-1])
+        {
+            int ans=ans=binary_search(nums,target,pivotindex+1,nums.size()-1);
+            return ans;
+        }
+         return -1;
+    
+    }
+};
