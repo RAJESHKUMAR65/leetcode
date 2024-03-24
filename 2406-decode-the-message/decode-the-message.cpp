37035e14a9f99b11f0516182cbf154252b74cfd7
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-03-24 15:01:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (84.03%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+      char start='a';
+      char mapping[280]={0};
+      
+      for(auto ch:key)
+      {
+        if(ch!=' ' && mapping[ch]==0)
+        {
+            mapping[ch]=start;
+            start++; 
+        }
+      }
+        string ans;
+        
+        for(auto ch:message)
+        {
+            if(ch==' ')
+            {
+                ans.push_back(ch);
+            }
+            else
+            {
+                ans.push_back(mapping[ch]);
+            }
+        }
+        
+      return ans;
+    
+    }
+};
