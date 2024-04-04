9b916edb5a9422225cccd6945b0919750e7c4fff
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-04-05 00:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (99.23%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
+ static string str;
+  static bool compare(char ch1,char ch2)
+   {
+      return(str.find(ch1)<str.find(ch2));
+   }
     string customSortString(string order, string s) {
-      int arr[26] ={0};
-      int count=0;
-      for(int i=0;i<s.length();i++)
-      {
-          int temp=s[i]-'a';
-          arr[temp]++;
-      }
-      while(!s.empty())
-      {
-          s.pop_back();
-      }
-      for(int i=0;i<order.length();i++)
-      {
-          int temp=order[i]-'a';
-          while(arr[temp]!=0)
-          {
-              s.push_back(order[i]);
-              arr[temp]--;
-              
-          }
-      }
-      for(int i=0;i<26;i++)
-      {
-          if(arr[i]!=0)
-          {
-              char ch=i+'a';
-              while(arr[i]!=0)
-              {
-                  s.push_back(ch);
-                  arr[i]--;
-              }
-              
-          }
-      }
-      return s;
-
+        str=order;
+        sort(s.begin(),s.end(),compare);
+        return s;
     }
+  
 };
+string Solution::str;
