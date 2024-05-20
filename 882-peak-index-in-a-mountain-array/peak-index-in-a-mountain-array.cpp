c0c4427e00da1b7becb62a2d95831daa5e9d43d4
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-05-20 15:38:10 +0530

    Time: 71 ms (92.29%) | Memory: 62 MB (44.00%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -5,15 +5,11 @@ public:
     int s=0;
     int e=arr.size()-1;
     int mid=s+(e-s)/2;
-    int ans=-1;
+  
     while(s<e)
     {
-        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-        {
-            ans=mid;
-            break;
-        }
-        else if(arr[mid]>arr[mid+1])
+        
+        if(arr[mid]>arr[mid+1])
         {
             e=mid;
         }
@@ -25,7 +21,7 @@ public:
     }
 
    
-       return ans; 
+       return s; 
     }
     
 };
