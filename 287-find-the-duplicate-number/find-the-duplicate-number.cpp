67ac0aa40aaa1b6fb71c0764c520214d57acc7c5
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-06-05 10:12:53 +0530

    Time: 66 ms (92.84%) | Memory: 63.5 MB (76.06%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-      int left = 0;
-        int right = nums.size() - 1;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int count = 0;
+        int ans=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+             int index=abs(nums[i]);
+             //already visited hei kya
+             if(nums[index]<0)
+             {
+                ans=index;
+                break;
+             }
+             // visited mark kr rahe hei
 
-            // Count the numbers less than or equal to mid
-            for (int num : nums) {
-                if (num <= mid) {
-                    count++;
-                }
-            }
-
-            // If count is greater than mid, the duplicate lies in the left half
-            if (count > mid) {
-                right = mid;
-            } else { // Otherwise, it lies in the right half
-                left = mid + 1;
-            }
+             nums[index]*=-1;
         }
 
-        return left;  
+        return ans;
     }
 };
