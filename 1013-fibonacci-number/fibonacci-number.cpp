c6d3613cca1262321acd508d9a9874857013d5b2
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-07-14 18:20:45 +0530

    Time: 10 ms (53.49%) | Memory: 7.5 MB (5.46%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -9,8 +9,25 @@ public:
 
         return ans;
     }
+    int topDown(int n,vector<int>&dp){
+          //base case
+        if(n==1 || n==0){
+            return n;
+        }
+        //step3:condition check kro kei answer phele sey exits nhi krta hei
+       if(dp[n]!=-1){
+        return dp[n];
+       }
+
+        //step2:jaha jaha ans store raha waha dp mai store kro and dp return kro 
+        dp[n]=recSolve(n-1)+recSolve(n-2);
+
+        return dp[n];
+    }
     int fib(int n) {
-        int ans =recSolve(n);
-        return ans;
+       //step 1:create dp array dekh n kaha sey ja raha hei
+       vector<int>dp(n+1,-1);
+       int ans=topDown(n,dp);
+       return ans;
     }
 };
