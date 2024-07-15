e1ed04ef5d6a4ccb46d7ee9a58c0937c41aeecf9
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-07-15 06:21:01 +0530

    Time: 3 ms (67.26%) | Memory: 7.6 MB (6.76%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -24,10 +24,38 @@ public:
 
         return dp[n];
     }
+    //bottom up approach
+    int bottomUp(int n){
+        //step1:create dp array
+        vector<int > dp(n+1,-1);
+        //step2:check the base case
+        dp[0]=0; if(n==0) return dp[0];
+        dp[1]=1;
+        //step3: phele n sey 2 tak calculate kr rahe the aab 2 sey n kei taraf jao
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    // space optimisation
+    int spaceOptimisation(int n){
+        int prev1=0;
+        int prev2=1;
+        if(n==0 || n==1){
+            return n;
+        }
+        int curr;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return curr;
+    }
     int fib(int n) {
        //step 1:create dp array dekh n kaha sey ja raha hei
        vector<int>dp(n+1,-1);
        int ans=topDown(n,dp);
-       return ans;
+       return spaceOptimisation( n);
     }
 };
