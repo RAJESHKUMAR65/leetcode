b89ce1193696b6b706b753de88a17e0b28bf13d5
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-07-27 11:51:07 +0530

    Time: 24 ms (78.57%) | Memory: 20.6 MB (49.32%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -39,6 +39,33 @@ public:
       dp[amount]=mini;
       return mini;// return dp[amount] bhi lekh skte hei
    }
+
+   // bottom of approach
+
+   int tabulation(vector<int>&coins,int amount){
+    //step 1:dp array create kro
+    vector<int>dp(amount+1,INT_MAX);// int_max isliye minimum chaye
+
+    //step2:base case check kro
+
+    dp[0]=0;
+    //step3: code ka follow dekho top downapproachmai
+    for(int target=1;target<=amount;target++){
+               int mini=INT_MAX;
+        for(int i=0;i<coins.size();i++){
+            if(target-coins[i]>=0){
+               int ans=dp[target-coins[i]];
+            if(ans!=INT_MAX){
+             mini=min(mini,ans+1);
+              }
+            }
+         
+      }
+      dp[target]=mini;
+      
+    }
+    return dp[amount];
+   }
     int coinChange(vector<int>& coins, int amount) {
         // int ans=solveRec(coins,amount);
         // if(ans!=INT_MAX){
@@ -49,7 +76,8 @@ public:
         // }
 
         vector<int >dp(amount+1,-1);
-        int ans=memo(coins,amount,dp);
+      //  int ans=memo(coins,amount,dp);
+      int ans=tabulation(coins,amount);
 
         if(ans!=INT_MAX){
             return ans;
