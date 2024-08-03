27cc6a3ec16fff09ba550282d13e84859b1c90ee
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-08-03 16:52:45 +0530

    Time: 72 ms (89.93%) | Memory: 62.5 MB (19.78%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        
-    int s=0;
-    int e=arr.size()-1;
-    int mid=s+(e-s)/2;
-  
-    while(s<e)
-    {
-        
-        if(arr[mid]>arr[mid+1])
-        {
-            e=mid;
-        }
-        else 
-         {
-            s=mid+1;
-         }
-        mid=s+(e-s)/2;
-    }
 
-   
-       return s; 
+    int binary_search(vector<int>& arr){
+        int s=0;
+        int n=arr.size();
+        int e=n-1;
+        int mid=s+(e-s)/2;
+       
+
+        while(s<=e){
+            if((mid-1>=0) && (mid+1)<n && arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if((mid+1<n )&& arr[mid]<arr[mid+1]){
+                s=mid+1;
+            }
+            else if((mid+1<n)  &&arr[mid]>arr[mid+1]){
+                e=mid-1;
+            }
+            mid=s+(e-s)/2;
+
+        }
+        return -1; 
     }
-    
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int ans=binary_search(arr);
+        return ans;
+        
+    }
 };
