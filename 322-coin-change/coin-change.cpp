06a328f947f78f28a5321773c33b28b3f4f91d8d
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-08-06 11:57:30 +0530

    Time: 22 ms (83.11%) | Memory: 20.6 MB (50.93%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,71 +1,71 @@
 class Solution {
 public:
-   int solveRec(vector<int>& coins,int amount){
-      //base case
-      if(amount ==0){
-        return 0;
-      }
-      if(amount<0){
-        return INT_MAX;
-      }
-        int mini=INT_MAX;
-      for(int i=0;i<coins.size();i++){
-          int ans=solveRec(coins,amount-coins[i]);
-          if(ans!=INT_MAX){
-             mini=min(mini,ans+1);
-          }
-      }
-      return mini;
-   }
-
-   // topdown approach
-   int memo(vector<int>&coins,int amount,vector<int>&dp){
-    if(amount ==0){
-        return 0;
-      }
-      if(amount<0){
-        return INT_MAX;
-      }
-      if(dp[amount]!=-1){
-        return dp[amount];
-      }
-        int mini=INT_MAX;
-      for(int i=0;i<coins.size();i++){
-          int ans=memo(coins,amount-coins[i],dp);
-          if(ans!=INT_MAX){
-             mini=min(mini,ans+1);
-          }
-      }
-      dp[amount]=mini;
-      return mini;// return dp[amount] bhi lekh skte hei
-   }
-
-   // bottom of approach
-
-   int tabulation(vector<int>&coins,int amount){
-    //step 1:dp array create kro
-    vector<int>dp(amount+1,INT_MAX);// int_max isliye minimum chaye
-
-    //step2:base case check kro
-
-    dp[0]=0;
-    //step3: code ka follow dekho top downapproachmai
-    for(int target=1;target<=amount;target++){
-               int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            if(target-coins[i]>=0){
-               int ans=dp[target-coins[i]];
-            if(ans!=INT_MAX){
-             mini=min(mini,ans+1);
-              }
+    int solveRec(vector<int>& coins, int amount) {
+        // base case
+        if (amount == 0) {
+            return 0;
+        }
+        if (amount < 0) {
+            return INT_MAX;
+        }
+        int mini = INT_MAX;
+        for (int i = 0; i < coins.size(); i++) {
+             int  ans = solveRec(coins, amount - coins[i]);
+            if (ans != INT_MAX) {
+                mini = min(mini, ans + 1);
             }
-         
-      }
-      dp[target]=mini;
-      
+        }
+        return mini;
     }
-    return dp[amount];
-   }
+
+    // topdown approach
+    int memo(vector<int>& coins, int amount, vector<int>& dp) {
+        if (amount == 0) {
+            return 0;
+        }
+        if (amount < 0) {
+            return INT_MAX;
+        }
+        if (dp[amount] != -1) {
+            return dp[amount];
+        }
+        int mini = INT_MAX;
+        for (int i = 0; i < coins.size(); i++) {
+            int ans = memo(coins, amount - coins[i], dp);
+            if (ans != INT_MAX) {
+                mini = min(mini, ans + 1);
+            }
+        }
+        dp[amount] = mini;
+        return mini; // return dp[amount] bhi lekh skte hei
+    }
+
+    // bottom of approach
+
+    int tabulation(vector<int>& coins, int amount) {
+        // step 1:dp array create kro
+        // dp[i ] is i amount ko create krne kei liye kei kitne minimum coint lagnege
+        vector<int> dp(amount + 1, INT_MAX); // int_max isliye minimum chaye
+
+        // step2:base case check kro
+
+        dp[0] = 0;
+        // step3: code ka follow dekho top downapproachmai
+        for (int target = 1; target <= amount; target++) {
+            int mini = INT_MAX;
+            // har ek amount bnane kei liye mai sare coins kei call krunga
+            for (int i = 0; i < coins.size(); i++) {
+                if (target - coins[i] >= 0) {
+                    int ans = dp[target - coins[i]];
+                    if (ans != INT_MAX) {
+                        mini = min(mini, ans + 1);
+                    }
+                }
+            }
+            dp[target] = mini;
+        }
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         // int ans=solveRec(coins,amount);
         // if(ans!=INT_MAX){
@@ -75,14 +75,13 @@ public:
         //     return -1;
         // }
 
-        vector<int >dp(amount+1,-1);
-      //  int ans=memo(coins,amount,dp);
-      int ans=tabulation(coins,amount);
+        vector<int> dp(amount + 1, -1);
+        //  int ans=memo(coins,amount,dp);
+        int ans = tabulation(coins, amount);
 
-        if(ans!=INT_MAX){
+        if (ans != INT_MAX) {
             return ans;
-        }
-        else{
+        } else {
             return -1;
         }
     }
