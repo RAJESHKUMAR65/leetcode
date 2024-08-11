a1fd7dafaaaf8718f124bb911b965e087b2f079e
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-08-12 00:19:42 +0530

    Time: 140 ms (61.27%) | Memory: 99.6 MB (21.63%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,62 +1,91 @@
 class Solution {
 public:
-    bool solveUsingRecursion(vector<int> &nums,int index,int sum){
+    bool solveUsingRecursion(vector<int>& nums, int index, int sum) {
 
-        if(index>=nums.size()){
+        if (index >= nums.size()) {
             return false;
         }
-        if(sum==0){
+        if (sum == 0) {
             return true;
         }
-        if(sum<0){
+        if (sum < 0) {
             return false;
         }
 
-        bool include=solveUsingRecursion(nums,index+1,sum-nums[index]);
-        bool exclude=solveUsingRecursion(nums,index+1,sum);
-        bool ans=(include || exclude);
+        bool include = solveUsingRecursion(nums, index + 1, sum - nums[index]);
+        bool exclude = solveUsingRecursion(nums, index + 1, sum);
+        bool ans = (include || exclude);
         return ans;
     }
-     bool solveUsingMemo(vector<int> &nums,int index,int target,vector<vector<int>>&dp){
+    bool solveUsingMemo(vector<int>& nums, int index, int target,
+                        vector<vector<int>>& dp) {
+        // target sum kuch bhi lekho
 
-        if(index>=nums.size()){
+        if (index >= nums.size()) {
             return false;
         }
-        if(target==0){
+        if (target == 0) {
             return true;
         }
-        if(target<0){
+        if (target < 0) {
             return false;
         }
-        if(dp[index][target]!=-1){
+        if (dp[index][target] != -1) {
             return dp[index][target];
         }
 
-        bool include=solveUsingMemo(nums,index+1,target-nums[index],dp);
-        bool exclude=solveUsingMemo(nums,index+1,target,dp);
-        dp[index][target]=(include || exclude);
+        bool include =
+            solveUsingMemo(nums, index + 1, target - nums[index], dp);
+        bool exclude = solveUsingMemo(nums, index + 1, target, dp);
+        dp[index][target] = (include || exclude);
         return dp[index][target];
     }
+
+    bool usingTabulation(vector<int>& nums, int target) {
+        int n = nums.size();
+        // step1 dp array create kro
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        // step2 base case dekho
+        for (int i = 0; i < nums.size(); i++) {
+            dp[i][0] = 1;
+        }
+
+        // index mera top down mai 0 sey n-1 ja raha hei
+        // toh yaha n-1 sey 0 jyega
+        for (int index = n - 1; index>= 0; index--) {
+            //
+            for (int t = 1; t <= target; t++) {
+                bool include=0;
+                if(t-nums[index]>=0){
+                   include =dp[index + 1][t - nums[index]];
+                }
+               
+                bool exclude = dp[index + 1][t];
+                dp[index][t] = (include || exclude);
+                
+            }
+        }
+        return dp[0][target];
+    }
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        int index=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+        int sum = 0;
+        int index = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
         }
         // yaha hei galati krunga
-        //jab sum odd hoga toh 2 equal subpart mai nhi divide kr paunga
-        if(sum &1){
+        // jab sum odd hoga toh 2 equal subpart mai nhi divide kr paunga
+        if (sum & 1) {
             return false;
         }
-        
-        int target=sum/2;
+
+        int target = sum / 2;
         // int ans =solveUsingRecursion(nums,index,target);
 
-        vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
-       int ans=solveUsingMemo(nums,index,target,dp);
+        // vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
+        // int ans = solveUsingMemo(nums, index, target, dp);
+        // return ans;
+        int ans=usingTabulation(nums,target);
         return ans;
-         
-
     }
-   
 };
