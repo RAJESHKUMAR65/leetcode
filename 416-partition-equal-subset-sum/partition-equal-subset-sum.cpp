9e35d9db4f8ca6c80907f5913e9097960f66f485
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-08-12 00:36:36 +0530

    Time: 94 ms (80.66%) | Memory: 14.1 MB (84.61%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -67,6 +67,37 @@ public:
         }
         return dp[0][target];
     }
+    bool usingspace(vector<int>& nums, int target) {
+        int n = nums.size();
+        // step1 dp array create kro
+        //vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        // step2 base case dekho
+        vector<int> curr(target+1,0);
+        vector<int> next(target+1,0);
+
+       //upper  column0 mai 1 dal rahe thi
+       curr[0]=1;
+       next[0]=1;
+        // index mera top down mai 0 sey n-1 ja raha hei
+        // toh yaha n-1 sey 0 jyega
+        for (int index = n - 1; index>= 0; index--) {
+            //
+            for (int t = 1; t <= target; t++) {
+                bool include=0;
+                if(t-nums[index]>=0){
+                   include =next[t - nums[index]];
+                }
+               
+                bool exclude = next[t];
+                curr[t] = (include || exclude);
+                
+            }
+            // shift yaad rakna yaha hum neeche sey upper ja rahe toh hume next
+            // current krna padega 
+            next=curr;
+        }
+        return next[target];
+    }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int index = 0;
@@ -85,7 +116,9 @@ public:
         // vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
         // int ans = solveUsingMemo(nums, index, target, dp);
         // return ans;
-        int ans=usingTabulation(nums,target);
+        // int ans=usingTabulation(nums,target);
+        // return ans;
+        int ans=usingspace(nums,target);
         return ans;
     }
 };
