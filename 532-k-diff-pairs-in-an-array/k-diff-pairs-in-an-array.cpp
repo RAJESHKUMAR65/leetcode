32748a9c946d6f75e3eda1019742830a7acc9f3c
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-08-27 18:30:29 +0530

    Time: 7 ms (95.19%) | Memory: 16.1 MB (41.57%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
+  
+    int binary_search(vector<int>&nums,int s,int e,int target){
+        int mid=s+(e-s)/2;
+        while(s<=e){
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+             mid=s+(e-s)/2;
+        }
+        return -1;
+    }
     int findPairs(vector<int>& nums, int k) {
+        // two pointer appraoch sey kra hein dekh lena hei
         sort(nums.begin(),nums.end());
-        set<pair<int,int>> ans;
-        int i=0;
-        int j=1;
-     
-        while(j<nums.size()){
-            int diff=abs(nums[j]-nums[i]);
-            if(diff==k){
-                ans.insert({nums[i],nums[j]});
-                cout<<nums[i]<<" "<<nums[j]<<endl;
-                i++;
-                j++;
-
-
-            }
-            else if(diff>k){
-                // diff kam krna hei toh isliye mein 
-                // number ko bada unga
-                i++;
-            }
-            else {// chhota hei toh 
-                   j++;
-            }
-            if(i==j){
-                j++;
+        set<pair<int,int>>ans;
+        for(int i=0;i<nums.size();i++){
+            int pair=binary_search(nums, i+1, nums.size()-1, k+nums[i]);
+            if(pair!=-1){
+                ans.insert({nums[i],nums[pair]});
             }
         }
+
         return ans.size();
-        
     }
 };
