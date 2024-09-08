648d2db468727548bceb761d24cf8a15163d2336
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-09-08 17:29:24 +0530

    Time: 29 ms (53.21%) | Memory: 15.2 MB (6.76%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -15,21 +15,47 @@ public:
         return ans;
     }
     int solveUsingMemo(vector<int>& arr, int start, int end,
-                            map<pair<int, int>, int>& maxi,vector<vector<int>>&dp) {
+                       map<pair<int, int>, int>& maxi,
+                       vector<vector<int>>& dp) {
         if (start == end)
             return 0;
-        int ans = INT_MAX;
-        if(dp[start][end]!=-1){
+
+        if (dp[start][end] != -1) {
             return dp[start][end];
         }
+        int ans = INT_MAX;
         for (int i = start; i < end; i++) {
             ans = min(ans, maxi[{start, i}] * maxi[{i + 1, end}] +
                                // start -- i-1 nhi jyega
-                               solveUsingMemo(arr, start, i, maxi,dp) +
-                               solveUsingMemo(arr, i + 1, end, maxi,dp));
+                               solveUsingMemo(arr, start, i, maxi, dp) +
+                               solveUsingMemo(arr, i + 1, end, maxi, dp));
         }
-         dp[start][end]=ans;
+        dp[start][end] = ans;
         return dp[start][end];
+    }
+    int solveUsingTab(vector<int>& arr, map<pair<int, int>, int>& maxi) {
+        int n = arr.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        
+        for (int start = n - 1; start >= 0; start--) {
+            for (int end = 0; end < n; end++) {
+                if (start >= end) {
+                    continue;
+                } else {
+                        int ans = INT_MAX;
+                        for (int i = start; i < end; i++) {
+                             ans = min(
+                            ans, maxi[{start, i}] * maxi[{i + 1, end}] +
+                                     // start -- i-1 nhi jyega
+                                     dp[start][i] +
+                                     dp[i + 1][end]);
+                          }
+                    dp[start][end] = ans;
+                   
+                }
+            }
+        }
+         return dp[0][n-1];
     }
     int mctFromLeafValues(vector<int>& arr) {
         // sare range ka maxi map mein store kra denge
@@ -41,9 +67,9 @@ public:
             }
         }
         int n = arr.size();
-       //int ans = solveUsingRecursion(arr, 0, n - 1, maxi);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans=solveUsingMemo(arr,0,n-1,maxi,dp);
+        // int ans = solveUsingRecursion(arr, 0, n - 1, maxi);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        int ans = solveUsingTab(arr,maxi);
 
         return ans;
     }
