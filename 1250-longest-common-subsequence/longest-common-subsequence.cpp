0b58a49b2a6e73b2e9b7426246f6005156c68310
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-09-08 22:13:20 +0530

    Time: 35 ms (31.16%) | Memory: 26.2 MB (9.30%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -24,10 +24,11 @@ public:
 
         }
         // agar character same hein
-          int ans=0;
+       
          if(dp[i][j]!=-1){
             return dp[i][j];
          }
+            int ans=0;
         if(text1[i]==text2[j]){
            ans=1+solveUsingMemo(text1,text2,i+1,j+1,dp);
         }
@@ -39,6 +40,24 @@ public:
         return dp[i][j];
     }
      
+  int solveUsingTab(string text1,string text2){
+    vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),0));
+    for(int i=text1.size()-1;i>=0;i--){
+        for(int j=text2.size()-1;j>=0;j--){
+                int ans=0;
+                if(text1[i]==text2[j]){
+                ans=1+dp[i+1][j+1];
+                }
+                else{
+                ans= 0+max(dp[i+1][j],dp[i][j+1]);
+                }
+                dp[i][j]=ans;
+
+               
+        }
+    }
+     return dp[0][0];
+  }
     int longestCommonSubsequence(string text1, string text2) {
         int i=0;
         int j=0;
