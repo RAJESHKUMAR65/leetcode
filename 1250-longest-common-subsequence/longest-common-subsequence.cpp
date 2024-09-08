f572a76320be9bbe77e42ebeda4821ea4bd850fd
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-09-08 23:21:35 +0530

    Time: 18 ms (83.78%) | Memory: 26.1 MB (22.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -53,17 +53,30 @@ public:
                         else{
                         ans= 0+max(dp[i+1][j],dp[i][j+1]);
                         }
-                        dp[i][j]=ans;
-
-            
-                
-                
-              
-
-               
-        }
+                        dp[i][j]=ans ;
+            }
     }
      return dp[0][0];
+  }
+  int solveUsingSo(string &text1,string &text2){
+    vector<int > curr(text2.size()+1,0);
+    vector<int> next(text2.size()+1,0);
+    for(int i=text1.size()-1;i>=0;i--){
+        for(int j=text2.size()-1;j>=0;j--){
+             
+                 int ans=0;
+                
+                        if(text1[i]==text2[j]){
+                        ans=1+next[j+1];
+                        }
+                        else{
+                        ans= 0+max(next[j],curr[j+1]);
+                        }
+                        curr[j]=ans ;
+            }
+            next=curr;
+    }
+     return curr[0];
   }
     int longestCommonSubsequence(string text1, string text2) {
         int i=0;
@@ -71,6 +84,6 @@ public:
        // return solveUsingRecursion(text1,text2,i,j);
         vector<vector<int>> dp(text1.length(),vector<int>(text2.length(),-1));
       //  return solveUsingMemo(text1,text2,i,j,dp);
-      return solveUsingTab(text1,text2);
+      return solveUsingSo(text1,text2);
     }
 };
