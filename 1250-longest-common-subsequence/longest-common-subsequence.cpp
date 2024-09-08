a27c19d042ac2e9df9a9ad7e7ed516f3de6b3c1b
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-09-08 22:13:41 +0530

    Time: 40 ms (23.02%) | Memory: 26.1 MB (33.19%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -40,7 +40,7 @@ public:
         return dp[i][j];
     }
      
-  int solveUsingTab(string text1,string text2){
+  int solveUsingTab(string &text1,string &text2){
     vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),0));
     for(int i=text1.size()-1;i>=0;i--){
         for(int j=text2.size()-1;j>=0;j--){
