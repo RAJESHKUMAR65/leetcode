9c435bf0d1ee6d324ecd22fcc3c44cdec078ffba
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-09-11 01:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (83.35%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,62 +1,41 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s[i];
-            if(ch=='{' || ch=='('|| ch=='[')
-            {// open bracket
-             st.push(ch);
-
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
             }
-            else
-            {
-                // closing bracket hoga
-                // is case ka hamesa dyan dena
-               // pop krne sey dekho kahi string empty toh nhi hei
-                if(!st.empty())
-                {
+            else{
+                //closing bracket
+                if(st.empty()){
+                    st.push(s[i]);
                     
-                
-                     if(ch==')' && st.top()=='(')
-                    {
-                        // match kr gaya
-                    st.pop();
-                    }
-                    else if(ch==']' && st.top()=='[')
-                    {
-                    st.pop();
-                    }
-                    else if(ch=='}' && st.top()=='{')
-                    {
-                    st.pop();
-                    }
-                    else
-                    {
-                        // nhi kr match
-                        return false;
-                    }
-
                 }
-                else
-                {
-                    // stack empty hei
-                    return false;
+                else{
+                    char ch=st.top();
+                    if(ch=='(' && s[i]==')'){
+                        st.pop();
+                    }
+                    else if(ch=='{' && s[i]=='}'){
+                        st.pop();
+                    }
+                    else if(ch=='[' && s[i]==']'){
+                        st.pop();
+                    }
+                    else{
+                        st.push(s[i]);
+                    }
                 }
-                
             }
         }
-        if(st.size()==0)
-        {
-            // sare operation kei baad string khale hogye
-            // valid parenthesis
+      
+        if(st.empty()){
             return true;
         }
-        else
-        {
+        else{
             return false;
         }
-        
+       
     }
 };
