752a465e0d73a9e1c49db15c6cf4a93107671acb
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-09-24 00:59:37 +0530

    Time: 117 ms (44.35%) | Memory: 94.4 MB (10.82%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,49 +1,65 @@
 class Solution {
 public:
-    
-    vector<int> nextSmallerElement(vector<int> &arr){
-        int n = arr.size();
-        vector<int>ans(n);
-        stack<int>st;
+    void prevSmallerElement(vector<int> &heights,vector<int> &prev){
+        stack<int> st;
         st.push(-1);
-        for(int i = 0;i<n;i++){
-            while(st.top()!= -1 && arr[st.top()]>=arr[i]){
+         
+        for(int i=0;i<heights.size();i++){
+            while(st.top()!=-1 && heights[st.top()]>=heights[i]){
                 st.pop();
             }
-            ans[i] = st.top();
+            prev.push_back(st.top());
             st.push(i);
-        }
-        return ans;
-    }
 
-    vector<int> prevSmallerElement(vector<int> &arr){
-        int n = arr.size();
-        vector<int>ans(n);
-        stack<int>st;
-        st.push(n);
-        for(int i = n-1;i>=0;i--){
-            while(st.top()!= n && arr[st.top()]>=arr[i]){
+        }
+         
+    }
+     void nextSmallerElement(vector<int> &heights,vector<int> &next){
+        stack<int> st;
+        st.push(heights.size());
+        
+        for(int i=heights.size()-1;i>=0;i--){
+            while(st.top()!=heights.size()&& heights[st.top()]>=heights[i]){
                 st.pop();
             }
-            ans[i] = st.top();
+            next.push_back(st.top());
             st.push(i);
-        }
-        return ans;
-    }
 
+        }
+        
+    }
     int largestRectangleArea(vector<int>& heights) {
+        int area=0;
+        vector<int> prev;
+        prevSmallerElement(heights,prev);
+        vector<int> next;
         
-        vector<int> left = nextSmallerElement(heights);
-        vector<int> right = prevSmallerElement(heights);
-        int n = heights.size();
-        int ans = 0;
-        for(int i = 0;i<n;i++){
-            // cout<<left[i]<<" "<<right[i]<<endl;;
-            int k = right[i]-left[i]-1;
-            // cout<<k*heights[i]<<endl;
-            ans = max(ans,k*heights[i]); 
-        }
-        return ans;
-        
+        nextSmallerElement(heights,next);
+        reverse(next.begin(),next.end());
+        // for(int i=0;i<prev.size();i++){
+        //     cout<<prev[i]<<" ";
+        // }
+        // cout<<endl;
+        // for(int i=0;i<next.size();i++){
+        //     cout<<next[i]<<" ";
+        // }
+        // cout<<endl;
+        // cout<<endl;
+        // cout<<prev.size()<<" "<<next.size()<<endl;
+
+     
+
+    for(int i=0;i<heights.size();i++){
+        int length=heights[i];
+        int width=next[i]-prev[i]-1;
+        // cout<<width<<" ";
+        // cout<<endl;
+        // cout<<endl;
+        int curr_area=length*width;
+        // cout<<curr_area<<" "<<i<<endl;
+        area=max(area,curr_area);
+    }
+
+        return area;
     }
 };
