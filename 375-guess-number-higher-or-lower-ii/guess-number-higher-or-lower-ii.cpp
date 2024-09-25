ad1fdd79d236bc2e33e3a7e966b2b4ac6271909d
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-09-25 19:34:03 +0530

    Time: 89 ms (12.45%) | Memory: 9.7 MB (71.65%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,74 +1,36 @@
 class Solution {
 public:
-    
-    int solveUsingRecursion(int start,int end){
-        if(start>=end){
-            // start == end means single elment hei us
-            // case mein penality nhi lage kei 
-            // start grater end hojyega matlab kei invalid range hei
-            return 0;
-        }
+   int solveUsingRecursion(int n,int start,int end){
 
-        int ans=INT_MAX;
-        for(int i=start;i<=end;i++){
-            // haar ek i mein jaha kr 
-            // max of uske left or right kei answer kei nikal  diya
-            // sare answer kei minimum nikal diya kyuki minimum pucha hein
-            // tracky question hei
-            ans=min(ans,i+max(solveUsingRecursion(start,i-1),solveUsingRecursion(i+1,end)));
-        }
-        return ans;
+    if(start>=end){
+        return 0;
     }
-    int solveUsingMem(int start,int end,vector<vector<int>>&dp){
-        if(start>=end){
-            // start == end means single elment hei us
-            // case mein penality nhi lage kei 
-            // start grater end hojyega matlab kei invalid range hei
-            return 0;
-        }
-        if(dp[start][end]!=-1){
-            return dp[start][end];
-        }
-
-        int ans=INT_MAX;
-        for(int i=start;i<=end;i++){
-            // haar ek i mein jaha kr 
-            // max of uske left or right kei answer kei nikal  diya
-            // sare answer kei minimum nikal diya kyuki minimum pucha hein
-            // tracky question hei
-            ans=min(ans,i+max(solveUsingMem(start,i-1,dp),solveUsingMem(i+1,end,dp)));
-        }
-        return dp[start][end]=ans;
+    int ans=INT_MAX;
+    for(int i=start;i<=end;i++){
+        ans=min(ans,i+max(solveUsingRecursion(n,start,i-1),solveUsingRecursion(n,i+1,end)));
     }
+    return ans;
+   }
+  
+   int solveUsingMemo(int n,int start,int end,vector<vector<int>>&dp){
 
-    int solveUsingtab(int start,int end){
-        int n=end;
-        vector<vector<int>>dp(n+2,vector(n+2,0));
-        for(int start=n;start>=1;start--){
-            for(int end=1;end<=n;end++){
-                  int ans=INT_MAX;
-                  if(start>=end){
-                    continue;
-                  }
-                  else{
-                     for(int i=start;i<=end;i++){
-                          ans=min(ans,i+max(dp[start][i-1],dp[i+1][end]));
-                        }
-                      dp[start][end]=ans;
-                  }
-                
-        }
-        
-        }
-        // kabhi kya return krna hei doubt ho toh intial fxn call dekh lena 
-        // tabulation wale fxn mein
-        return dp[1][n];
-        }
-    
+    if(start>=end){
+        return 0;
+    }
+    if(dp[start][end]!=-1){
+        return dp[start][end];
+    }
+    int ans=INT_MAX;
+    for(int i=start;i<=end;i++){
+        ans=min(ans,i+max(solveUsingMemo(n,start,i-1,dp),solveUsingMemo(n,i+1,end,dp)));
+    }
+   dp[start][end]=ans;
+   return dp[start][end];
+   }
     int getMoneyAmount(int n) {
-      // int ans = solveUsingRecursion(1,n);
-      vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-      int ans=solveUsingtab(1,n);
-       return ans; 
+       // int ans=solveUsingRecursion(n,1,n);
+       vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+       int ans=solveUsingMemo(n,0,n,dp);
+        return ans;
     }
 };
