898e0415371390c67d491736229d1fe43e0d3f00
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-09-25 23:23:04 +0530

    Time: 34 ms (91.25%) | Memory: 11.6 MB (5.51%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -28,20 +28,24 @@ public:
    return dp[start][end];
    }
    int solveUsingTab(int n,int start,int end){
-    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    vector<vector<int>>dp(n+2,vector<int>(n+2,0));
 
-    for(int start=n;start>=0;start--){
-        for(int end=0;end<=n;end++){
-        if(start<end){
-               
-            int ans=INT_MAX;
-          for(int i=start;i<=end;i++){
-           ans=min(ans,i+max(solveUsingMemo(n,start,i-1,dp),solveUsingMemo(n,i+1,end,dp)));}
-          dp[start][end]=ans;
+    for(int start=n;start>=1;start--){
+        for(int end=1;end<=n;end++){
+        if(start>=end){
+           continue;
         }
-     }
+        else{ 
+            int ans=INT_MAX;
+            for(int i=start;i<=end;i++){
+            ans=min(ans,i+max(dp[start][i-1],dp[i+1][end]));}
+            dp[start][end]=ans;
+            }
+               
+        }
+      
     }
-    return dp[0][n];
+    return dp[1][n];
 
    }
     int getMoneyAmount(int n) {
