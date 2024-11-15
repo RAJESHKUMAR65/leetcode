a6503a0b1363f2ecb19fa062adf7b6e64d633ff8
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-11-15 16:57:26 +0530

    Time: 182 ms (17.92%) | Memory: 12.8 MB (23.75%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> perfectSquare(int n){
+    vector<int> perfect_array;
+      for(int i=1;i<=n;i++){
+            int j=sqrt(i);
+            if(j*j==i){
+               perfect_array.push_back(i);
+            }
+      }
+      return perfect_array;
+    }
+    int solveUsingRecursion(vector<int>&arr,int n,vector<int>&dp){
+          if(n==0){
+            return 0;
+          }
+          if(n<0){
+            return INT_MAX;
+          }
+         if(dp[n]!=-1){
+            return dp[n];
+
+         }
+         
+          int final=INT_MAX;
+          for(int i=0;i<arr.size();i++){
+            int ans=solveUsingRecursion(arr,n-arr[i],dp);
+            if(ans!=INT_MAX){
+                 final=min(final,ans+1);
+            }
+          }
+          dp[n]=final;
+          return dp[n];
+    }
+   
+    int numSquares(int n) {
+        vector<int>dp(n+1,-1);
+        vector<int> arr=perfectSquare(n);
+        int ans=solveUsingRecursion(arr,n,dp);
+        return ans;
+       
+    }
+};
