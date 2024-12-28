e864f3569b4bf19eb8d88bbb32ffa87cd575255b
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-12-28 11:28:57 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (16.57%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
+  vector<int> getLeftMax(vector<int>&height){
+    vector<int>leftMax(height.size());
+    leftMax[0]=height[0];
+    for(int i=1;i<height.size();i++){
+        leftMax[i]=max(leftMax[i-1],height[i]);
+    }
+    return leftMax;
+  }
+
+  vector<int> getRightMax(vector<int>&height){
+    int n=height.size();
+    vector<int>rightMax(height.size());
+    rightMax[n-1]=height[n-1];
+    for(int i=n-2;i>=0;i--){
+        rightMax[i]=max(rightMax[i+1],height[i]);
+    }
+    return rightMax;
+  }
     int trap(vector<int>& height) {
-         int n=height.size();
-        vector<int> left(n);
-        vector<int> right(n);
-        int water=0;
-      
-        left[0]=height[0];
-        right[n-1]=height[n-1];
-        for(int i=1;i<n;i++)
-        {
-            left[i]=max(height[i],left[i-1]);
+        vector<int>left_max=getLeftMax(height);
+        vector<int>right_max=getRightMax(height);
+        int sum=0;
+        for(int i=0;i<height.size();i++){
+          // int width=1; doesnt effect
+          int h=min(left_max[i],right_max[i])-height[i];
+          sum+=h;
         }
-        for(int i=n-2;i>=0;i--)
-        {
-            right[i]=max(height[i],right[i+1]);
-        }
-        for(int i=1;i<n-1;i++)
-        {
-            water+=min(right[i],left[i])-height[i];
-        }
-        return water;
-        
-        
+        return sum;
+
     }
 };
