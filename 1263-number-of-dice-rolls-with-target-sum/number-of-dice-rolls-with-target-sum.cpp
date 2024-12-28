564ca4591e57b01d46e2d2d88bfc0012de49dd43
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2024-12-28 23:21:34 +0530

    Time: 15 ms (59.20%) | Memory: 11.1 MB (49.61%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,75 +1,46 @@
 class Solution {
 public:
- const int mod=1e9+7;
-  int recursion(int n,int k ,int target){
-     
-     if(target<0){
-        return 0;
-     }
-     if(n==0 && target==0){
-        return 1;
-     }
-     if(n==0 && target!=0){
-        return 0;
-     }
-     if(n!=0 && target==0){
-        return 0;
-     }
-     int ans=0;
-     for(int i=1;i<=k;i++){
-         ans= ans+recursion(n-1,k,target-i);
-    }
-    return ans;
-  }
-  long long solveUsingMemo(int n,int k,int target,vector<vector<int>>&dp){
-     if(target<0){
-        return 0;
-     }
-     if(n==0 && target==0){
-        return 1;
-     }
-     if(n==0 && target!=0){
-        return 0;
-     }
-     if(n!=0 && target==0){
-        return 0;
-     }
-     if(dp[n][target]!=-1){
-        return dp[n][target];
-     }
-     long long ans=0;
-     for(int i=1;i<=k;i++){
-         ans= (ans+solveUsingMemo(n-1,k,target-i,dp)) % mod;
-    }
-      dp[n][target]=ans;
-    return (dp[n][target]%mod);
-  
-  }
-  int solveUsingtab(int n,int k ,int target){
-    vector<vector<int>>dp(n+1,vector<int>(target+1,0));
-    dp[0][0]=1;
-    for(int dice=1;dice<=n;dice++){
-        for(int j=1;j<=target;j++){
-            long long ans=0;
-           for(int i=1;i<=k;i++){
-            if(j-i>=0){
-                ans= (ans%mod+dp[dice-1][j-i]%mod )% mod;
-            }
-              
-           }
-          dp[dice][j]=ans;
-    
-        }
+  int mod=1e9+7;
+    int solveUsingRecursion(int n,int k,int target) {
+         if(target<0){
+            return 0;
+         }
+         if(n==0 && target!=0){
+            return 0;
+         }
+         if(n==0 && target==0){
+            return 1;
+         }
+         int ans=0;
+         for(int i=1;i<=k;i++){
+           ans+= solveUsingRecursion(n-1,k,target-i);
+         }
+         return ans;
         
     }
-    return dp[n][target];
-  }
+    int solveUsingMemo(int n,int k,int target,vector<vector<int>>&dp) {
+         if(target<0){
+            return 0;
+         }
+         if(n==0 && target!=0){
+            return 0;
+         }
+         if(n==0 && target==0){
+            return 1;
+         }
+         if(dp[n][target]!=-1){
+            return dp[n][target];
+         }
+         int ans=0;
+         for(int i=1;i<=k;i++){
+           ans= (ans+solveUsingMemo(n-1,k,target-i,dp))%mod;
+         }
+         return dp[n][target]=ans%mod;
+        
+    }
     int numRollsToTarget(int n, int k, int target) {
-       vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-    //   int ans=recursion(n,k,target);
-    //   return ans;
-        //  
-    int ans=solveUsingtab(n, k,target);
-    return ans;
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        int ans=solveUsingMemo(n,k,target,dp);
+        return ans;
     }
 };
