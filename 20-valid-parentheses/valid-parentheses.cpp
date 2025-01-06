26f83b2abb4ff218019507ae22efcb2356b471ae
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-01-06 13:37:33 +0530

    Time: 3 ms (7.23%) | Memory: 10.1 MB (5.66%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
+        stack<int>st;
         for(int i=0;i<s.length();i++){
-            if(s[i]=='(' || s[i]=='{'||s[i]=='['){
-                st.push(s[i]);
+            char ch=s[i];
+            if(ch=='(' || ch=='{'||ch=='['){
+                st.push(ch);
             }
             else{
-                //closing bracket
                 if(st.empty()){
-                    st.push(s[i]);
-                    
+                    return false;
                 }
                 else{
-                    char ch=st.top();
-                    if(ch=='(' && s[i]==')'){
+                    int topelement=st.top();
+                    if(ch==')'&& topelement=='('){
                         st.pop();
                     }
-                    else if(ch=='{' && s[i]=='}'){
-                        st.pop();
-                    }
-                    else if(ch=='[' && s[i]==']'){
+                   else if(ch=='}'&& topelement=='{'){
+                       st.pop();}
+                    else if(ch==']' && topelement=='['){
                         st.pop();
                     }
                     else{
-                        st.push(s[i]);
+                        return false;
                     }
-                }
             }
         }
       
-        if(st.empty()){
+    }
+      if(st.empty()){
             return true;
         }
         else{
             return false;
         }
-       
-    }
+
+}
 };
