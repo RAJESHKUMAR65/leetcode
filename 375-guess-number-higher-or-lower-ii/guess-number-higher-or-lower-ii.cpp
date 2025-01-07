ea7c1687d124c2cadb86a4498c9a868b2bf4cda7
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-01-07 11:08:38 +0530

    Time: 89 ms (7.86%) | Memory: 11.1 MB (17.81%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,57 +1,34 @@
 class Solution {
 public:
-   int solveUsingRecursion(int n,int start,int end){
-
-    if(start>=end){
-        return 0;
+   int solveUsingRecursion(int n,int s,int e){
+    if(s>=e){
+         return 0;
     }
+
     int ans=INT_MAX;
-    for(int i=start;i<=end;i++){
-        ans=min(ans,i+max(solveUsingRecursion(n,start,i-1),solveUsingRecursion(n,i+1,end)));
+    for(int i=s;i<=e;i++){
+        ans=min(ans,i+max(solveUsingRecursion(n,s,i-1),solveUsingRecursion(n,i+1,e)));
     }
     return ans;
    }
-  
-   int solveUsingMemo(int n,int start,int end,vector<vector<int>>&dp){
+   int solveUsingMemo(int n,int s,int e,vector<vector<int>>&dp){
+        if(s>=e){
+         return 0;
+     }
 
-    if(start>=end){
-        return 0;
-    }
-    if(dp[start][end]!=-1){
-        return dp[start][end];
-    }
     int ans=INT_MAX;
-    for(int i=start;i<=end;i++){
-        ans=min(ans,i+max(solveUsingMemo(n,start,i-1,dp),solveUsingMemo(n,i+1,end,dp)));
+    if(dp[s][e]!=-1){
+        return dp[s][e];
     }
-   dp[start][end]=ans;
-   return dp[start][end];
-   }
-   int solveUsingTab(int n,int start,int end){
-    vector<vector<int>>dp(n+2,vector<int>(n+2,0));
-
-    for(int start=n;start>=1;start--){
-        for(int end=1;end<=n;end++){
-        if(start>=end){
-           continue;
-        }
-        else{ 
-            int ans=INT_MAX;
-            for(int i=start;i<=end;i++){
-            ans=min(ans,i+max(dp[start][i-1],dp[i+1][end]));}
-            dp[start][end]=ans;
-            }
-               
-        }
-      
+    for(int i=s;i<=e;i++){
+        ans=min(ans,i+max(solveUsingMemo(n,s,i-1,dp),solveUsingMemo(n,i+1,e,dp)));
     }
-    return dp[1][n];
-
+    return dp[s][e]= ans;
    }
     int getMoneyAmount(int n) {
-       // int ans=solveUsingRecursion(n,1,n);
-       vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-       int ans=solveUsingTab(n,0,n);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        
+        int ans=solveUsingMemo(n,1,n,dp);
         return ans;
     }
 };
