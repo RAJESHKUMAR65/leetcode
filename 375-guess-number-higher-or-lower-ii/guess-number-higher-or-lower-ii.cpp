912cabf3e84a7d88ee79c4967239a33052da6d96
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-01-07 12:12:00 +0530

    Time: 35 ms (83.31%) | Memory: 13.3 MB (5.39%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-   int solveUsingRecursion(int n,int s,int e){
+   int solveUsingRecursion(int s,int e){
     if(s>=e){
          return 0;
     }
 
     int ans=INT_MAX;
     for(int i=s;i<=e;i++){
-        ans=min(ans,i+max(solveUsingRecursion(n,s,i-1),solveUsingRecursion(n,i+1,e)));
+        ans=min(ans,i+max(solveUsingRecursion(s,i-1),solveUsingRecursion(i+1,e)));
     }
     return ans;
    }
-   int solveUsingMemo(int n,int s,int e,vector<vector<int>>&dp){
+   int solveUsingMemo(int s,int e,vector<vector<int>>&dp){
         if(s>=e){
          return 0;
      }
@@ -21,14 +21,39 @@ public:
         return dp[s][e];
     }
     for(int i=s;i<=e;i++){
-        ans=min(ans,i+max(solveUsingMemo(n,s,i-1,dp),solveUsingMemo(n,i+1,e,dp)));
+        ans=min(ans,i+max(solveUsingMemo(s,i-1,dp),solveUsingMemo(i+1,e,dp)));
     }
     return dp[s][e]= ans;
    }
+   int solveUsingTab(int n){
+    vector<vector<int>>dp(n+3,vector<int>(n+4,0));
+    for(int s=n;s>=1;s--){
+        for(int e=1;e<=n;e++){
+            int ans=INT_MAX;
+            if(s>=e){
+                continue;
+            }
+            else{
+                  for(int i=s;i<=e;i++){
+            
+                     ans=min(ans,i+max(dp[s][i-1],dp[i+1][e]));
+                }
+
+            }
+          
+             
+              
+              dp[s][e]= ans;
+        }
+
+        }
+        return dp[1][n];
+    }
+   
     int getMoneyAmount(int n) {
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         
-        int ans=solveUsingMemo(n,1,n,dp);
+        int ans=solveUsingTab(n);
         return ans;
     }
 };
