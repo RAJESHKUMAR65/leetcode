66c1346bd30ad35e10ae835febb6810cce5dc27b
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-01-08 00:17:26 +0530

    Time: 50 ms (83.67%) | Memory: 76 MB (15.65%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
-int solveUsingRecursion(string s,int i,int j){
-    if(i>j){
+   int solveUsingRecursion(string &s,int i,int j){
+     if(i>j){
         return 0;
-    }
-    if(i==j && s[i]==s[j]){
-        return 1;
-    }
-    int ans=INT_MIN;
-
-    if(s[i]==s[j]){
-        ans=2+solveUsingRecursion(s,i+1,j-1);
-    }
-    else{
+     }
+     int ans=0;
+     if(s[i]==s[j]){
+       ans=2+solveUsingRecursion(s,i+1,j-1);
+     }
+     else{
         ans=0+max(solveUsingRecursion(s,i+1,j),solveUsingRecursion(s,i,j-1));
-    }
-    return ans;
-}
-int solveUsingMemo(string &s,int i,int j,vector<vector<int>>&dp){
-    if(i>j){
+     }
+     return ans;
+   }
+   int solveUsingMemo(string &s,int i,int j,vector<vector<int>>&dp){
+     if(i>j){
         return 0;
-    }
-    if(i==j && s[i]==s[j]){
+     }
+     if(i==j){
         return 1;
-    }
-
-    if(dp[i][j]!=-1){
+     }
+     int ans=0;
+     if(dp[i][j]!=-1){
         return dp[i][j];
-    }
-    int ans=INT_MIN;
+     }
+     if(s[i]==s[j]){
+       ans=2+solveUsingMemo(s,i+1,j-1,dp);
+     }
+     else{
+        ans=0+max(solveUsingMemo(s,i+1,j,dp),solveUsingMemo(s,i,j-1,dp));
+     }
+     return dp[i][j]=ans;
 
-    if(s[i]==s[j]){
-         ans=2+solveUsingMemo(s,i+1,j-1,dp);
-    }
-    else{
-          ans=0+max(solveUsingMemo(s,i+1,j,dp), solveUsingMemo(s,i,j-1,dp));
-      }
-      dp[i][j]=ans;
-
-      return dp[i][j];
-
-}
+   }
     int longestPalindromeSubseq(string s) {
-       if(s.length()==1){
-        return 1;
-       }
         int i=0;
         int j=s.length()-1;
-        //return solveUsingRecursion(s,i,j);
-        vector<vector<int>>dp(s.length(),vector<int>(s.length(),-1));
-        return solveUsingMemo(s,i,j,dp);
+        
+        int n=s.length();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int ans=solveUsingMemo(s,i,j,dp);
+        return ans;
     }
 };
