7ba580a2ebe6a1f61f8083934b5001061f6a4f8b
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-01-12 14:33:29 +0530

    Time: 41 ms (41.39%) | Memory: 19 MB (49.95%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,55 +1,48 @@
 class Solution {
 public:
-   int solveUsingRecursion(vector<int>&coins,int amount,vector<int> &dp){
-    if(amount ==0){
+ int solveUsingRecursion(vector<int>&coins,int target){
+    if(target==0){
         return 0;
     }
-    if(amount<0){
-        return INT_MAX;
-
+    if(target<0){
+       return INT_MAX;
     }
-    if(dp[amount]!=-1){
-        return dp[amount];
-    }
-     
-    int final=INT_MAX;
+    int finalans=INT_MAX;
     for(int i=0;i<coins.size();i++){
-        int ans=solveUsingRecursion(coins,amount-coins[i],dp);
-        if(ans!=INT_MAX){
-            final=min(final,ans+1);
-        }
+     int ans= solveUsingRecursion(coins,target-coins[i]);
+       if(ans!=INT_MAX){
+          finalans=min(finalans,ans+1);
+       }
     }
-    dp[amount]=final;
-    return final;
-   }
-   int solveUsingtab(vector<int>&coins,int amount){
-    vector<int >dp(amount+1,INT_MAX);
-    dp[0]=0;
-    for(int i=0;i<=amount;i++){
-            int final=INT_MAX;
-    for(int j =0;j<coins.size();j++){
-        if(i-coins[j]>=0){
-               int ans=dp[i-coins[j]];
-           if(ans!=INT_MAX){
-            final=min(final,ans+1);
-              dp[i]=final;
+    return finalans;
+ }
+ int  solveUsingMemo(vector<int>&coins,int target,vector<int>&dp){
+    if(target==0){
+        return 0;
     }
-        }
-     }
-  
+    if(target<0){
+       return INT_MAX;
     }
-    return dp[amount];
-   }
-     
+    if(dp[target]!=-1){
+        return dp[target];
+    }
+    int finalans=INT_MAX;
+    for(int i=0;i<coins.size();i++){
+     int ans=  solveUsingMemo(coins,target-coins[i],dp);
+       if(ans!=INT_MAX){
+          finalans=min(finalans,ans+1);
+       }
+    }
+    return dp[target]=finalans;
+ }
     int coinChange(vector<int>& coins, int amount) {
-        
         vector<int>dp(amount+1,-1);
-        int ans=solveUsingtab(coins,amount);
+        int ans=solveUsingMemo(coins,amount,dp);
         if(ans!=INT_MAX){
-          return ans;
-        }else{
+            return ans;
+        }
+        else{
             return -1;
         }
-       
     }
 };
