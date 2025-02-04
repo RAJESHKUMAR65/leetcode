45bcccfbc8f846d344214759cfc39b140fdaa334
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-02-04 22:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (68.22%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    void reverse(vector<int> &matrix)
-    {
-        int s=0;
-        int e=matrix.size()-1;
-        while(s<=e)
-        {
-            swap(matrix[s],matrix[e]);
-            s++;
-            e--;
-        }
-    }
     void rotate(vector<vector<int>>& matrix) {
-        // phele tranpose kiya matrix ko
-        for(int i=0;i<matrix.size();i++)
-        {
-            for(int j=i;j<matrix[i].size();j++)
-            {
-                swap(matrix[i][j],matrix[j][i]);
+       // transpose krna padega
+       for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[i].size();j++){
+            if(j>=i){
+              swap(matrix[i][j],matrix[j][i]);
             }
         }
-        // pr hr row ko reverse kiya 
-    for(int i=0;i<matrix.size();i++)
-    {
-        reverse(matrix[i]);
-    }
+       } 
+       for(int i=0;i<matrix.size();i++){
+           reverse(matrix[i].begin(),matrix[i].end());
+       }
     }
 };
