07240bf1ba742a9820aa8e78f12a2a7aa8d4cd8f
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-03-08 18:34:37 +0530

    Time: 20 ms (5.79%) | Memory: 21.9 MB (98.34%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,56 +1,29 @@
 class Solution {
 public:
- bool ispalindrome(string s,int i,int j)
- {
-    
-    while(i<=j)
-    {
-        if(s[i]==s[j])
-        {
+bool checkpalindrome(int i,int j,string &s){
+    while(i<=j){
+        if(s[i]==s[j]){
             i++;
             j--;
         }
-        else
-        {
+        else{
             return false;
         }
     }
     return true;
- }
+}
     bool validPalindrome(string s) {
-       int i=0;
-       int j=s.length()-1;
-       while(i<=j)
-       {
-        if(s[i]==s[j])
-        {
-            i++;
-            j--;
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else{
+               return  checkpalindrome(i+1,j,s) || checkpalindrome(i,j-1,s);
+            }
         }
-        else
-        {
-            // remove krna hei
-            // i wala remove kiya
-            
-            if(ispalindrome(s,i+1,j))
-            {
-                 
-                return true;
-            }
-            else if(ispalindrome(s,i,j-1))
-            {
-                 
-                return true;
-                
-            }
-            else
-            {
-                return false;
-            }
-            
-
-        }
-       } 
-       return   true;
+        return true;
     }
 };
