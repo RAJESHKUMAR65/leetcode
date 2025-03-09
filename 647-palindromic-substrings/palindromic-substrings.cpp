d92e7329635ad9fd01aa18307feb1a8120c17899
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-03-09 16:04:05 +0530

    Time: 520 ms (14.09%) | Memory: 8.6 MB (81.41%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-int expand(string s,int i,int j)
-{
-    int count=0;
-    while(i>=0 && j<s.length() && s[i]==s[j])
-    {
-        count++;
-        i--;
-        j++;
-    }
-    return count;
-}
-  
- 
-    int countSubstrings(string s) {
-        int totalcount=0;
-        for(int centre=0;centre<s.length();centre++)
-        {
-            // odd substring alag handle
-            int oddkaAns=expand(s,centre,centre);
-            // even substring alag handle 
-            int evenkaAns=expand(s,centre,centre+1);
-            totalcount +=oddkaAns+evenkaAns;
+   bool checkPalindrome(int i,int j,string &s){
+    while(i<=j){
+        if(s[i]==s[j]){
+            i++;
+            j--;
         }
-        
-        return totalcount;
-
+        else{
+            return false;
+        }
     }
+    return true;
+   }
+    int countSubstrings(string s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                  if(checkPalindrome(i,j,s)) count++;
+            }
+        }
+        return count;
+    }
+     
 };
