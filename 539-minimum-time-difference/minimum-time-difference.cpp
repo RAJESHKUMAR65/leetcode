ce349e5fc46f6784b7e18dd2fd12907dc938c37b
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-03-09 15:53:20 +0530

    Time: 11 ms (14.85%) | Memory: 18.9 MB (24.63%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -1,50 +1,37 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        int Time[timePoints.size()];
-    for (int i=0;i<timePoints.size();i++)
-    {
-        string s=timePoints[i];
-        s.erase(2,1);
-       
-        int con=stoi(s);
-        int hour= con/100;
-        int minute=con%100;
-        int ans =(hour*60)+minute;
-        Time[i]=ans;;
-    }
-     
-     sort(Time,Time+timePoints.size());
-     int mini=INT_MAX;
-     for(int i=0;i<timePoints.size();i++)
-     {
-        if(i+1<timePoints.size())
-       {
-         int a=Time[i+1]-Time[i];
-         if(a<mini)
-         {
-            mini=a;
-         }
-        
-        
+        int miniTimediff=INT_MAX;
+        sort(timePoints.begin(),timePoints.end());
+        for(int i=0;i<=timePoints.size()-2;i++){
+            string str1=timePoints[i];
+            string str2=timePoints[i+1];
+            int hours1=stoi(str1.substr(0,2));
+            int minute1=stoi(str1.substr(3,2));
+            int timeMin1=hours1*60+minute1;
+            int hours2=stoi(str2.substr(0,2));
+            int minute2=stoi(str2.substr(3,2));
+            int timeMin2=hours2*60+minute2;
 
-       }
-        
-     }
+            int diff=abs(timeMin1-timeMin2);
+            
+            miniTimediff=min(diff,miniTimediff);
+            }
 
-      
-    
-    int n=timePoints.size()-1;
-    int last=(Time[0]+1440)-Time[n];
-    cout<<last;
-    
-    if(last<mini)
-    {
-        return last;
-    }
-    else
-    {
-        return mini;
-    }
+            string str1=timePoints[0];
+            string str2=timePoints[timePoints.size()-1];
+            int hours1=stoi(str1.substr(0,2));
+            int minute1=stoi(str1.substr(3,2));
+            int timeMin1=(hours1*60+minute1)+1440;
+            int hours2=stoi(str2.substr(0,2));
+            int minute2=stoi(str2.substr(3,2));
+            int timeMin2=hours2*60+minute2;
+
+            int diff=abs(timeMin1-timeMin2);
+            
+            miniTimediff=min(diff,miniTimediff);
+
+
+       return miniTimediff;
     }
 };
