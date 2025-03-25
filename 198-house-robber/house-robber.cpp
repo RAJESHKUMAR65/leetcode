cfb2adfcb6d54e6375eec4669212893f3dae6478
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-03-26 01:46:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.47%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-   int solveUsingRecursion(vector<int>&nums,int i){
+
+  int rec(vector<int>&nums,int i){
     if(i>=nums.size()){
         return 0;
     }
-    int include=nums[i]+solveUsingRecursion(nums,i+2);
-    int exclude=0+solveUsingRecursion(nums,i+1);
-    int ans=max(include,exclude);
-    return ans;
-   }
+    int include=nums[i]+rec(nums,i+2);
+    int exclude=0+rec(nums,i+1);
+    return max(include,exclude);
+  }
    int memo(vector<int>&nums,int i,vector<int>&dp){
     if(i>=nums.size()){
         return 0;
@@ -18,33 +18,12 @@ public:
     }
     int include=nums[i]+memo(nums,i+2,dp);
     int exclude=0+memo(nums,i+1,dp);
-    int ans=max(include,exclude);
-    dp[i]=ans;
-    return ans;
-   }
-   int bottomup(vector<int>&nums){
-    int next=0;
-    int curr=0;
-    int prev=0;
-    for(int i=nums.size()-1;i>=0;i--){
-        
-            int include=nums[i]+next;
-            int exclude=0+curr;
-            int ans=max(include,exclude);
-            prev=ans;
-           next =curr;
-           curr=prev;
-
-        }
-        return prev;
-       
-    
-    
-   }
-   
+    return dp[i]=max(include,exclude);
+  }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        int ans= bottomup(nums);
+        vector<int>dp(nums.size()+1,-1);
+        int i=0;
+        int ans=memo(nums,i,dp);
         return ans;
     }
 };
