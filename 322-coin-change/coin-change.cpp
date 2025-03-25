17afd8ba2b5c9fd2e1505138d316f48589059798
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-03-26 01:34:42 +0530

    Time: 36 ms (46.50%) | Memory: 19.2 MB (48.13%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
- int solveUsingRecursion(vector<int>&coins,int target){
-    if(target==0){
-        return 0;
+int rec(vector<int>&coins,int amount){
+    
+    if(amount<0){
+        return INT_MAX;
     }
-    if(target<0){
-       return INT_MAX;
+    if(amount==0){
+        return 0;
     }
     int finalans=INT_MAX;
     for(int i=0;i<coins.size();i++){
-     int ans= solveUsingRecursion(coins,target-coins[i]);
-       if(ans!=INT_MAX){
-          finalans=min(finalans,ans+1);
-       }
+         int ans=rec(coins,amount-coins[i]);
+         if(ans!=INT_MAX){
+            finalans=min(finalans,ans+1);
+         }
     }
     return finalans;
- }
- int  solveUsingMemo(vector<int>&coins,int target,vector<int>&dp){
-    if(target==0){
+}
+int memo(vector<int>&coins,int amount,vector<int>&dp){
+    
+    if(amount<0){
+        return INT_MAX;
+    }
+    if(amount==0){
         return 0;
     }
-    if(target<0){
-       return INT_MAX;
-    }
-    if(dp[target]!=-1){
-        return dp[target];
+    if(dp[amount]!=-1){
+        return dp[amount];
     }
     int finalans=INT_MAX;
     for(int i=0;i<coins.size();i++){
-     int ans=  solveUsingMemo(coins,target-coins[i],dp);
-       if(ans!=INT_MAX){
-          finalans=min(finalans,ans+1);
-       }
+         int ans=memo(coins,amount-coins[i],dp);
+         if(ans!=INT_MAX){
+            finalans=min(finalans,ans+1);
+         }
     }
-    return dp[target]=finalans;
- }
+    return dp[amount]=finalans;
+}
     int coinChange(vector<int>& coins, int amount) {
         vector<int>dp(amount+1,-1);
-        int ans=solveUsingMemo(coins,amount,dp);
+        int ans=memo(coins,amount,dp);
         if(ans!=INT_MAX){
             return ans;
         }
-        else{
-            return -1;
-        }
+        return -1;
     }
 };
