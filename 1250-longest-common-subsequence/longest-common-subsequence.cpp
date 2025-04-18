f2313b9a1b5952742a3f1e34c05897a835837874
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-04-18 22:54:06 +0530

    Time: 2220 ms (5.14%) | Memory: 456.6 MB (6.71%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,82 +1,46 @@
 class Solution {
 public:
-    int solveUsingRecursion(string text1,string text2,int i,int j){
-        if(i==text1.length()){
-            return 0;
-        }
-        if(j==text2.length()){
-            return 0;
-        }
-        int ans=INT_MIN;
-        if(text1[i]==text2[j]){
-            ans=1+solveUsingRecursion(text1,text2,i+1,j+1);
-        }
-        else{
-            ans=0+max(solveUsingRecursion(text1,text2,i,j+1),solveUsingRecursion(text1,text2,i+1,j));
-        }
-        return ans;
+ int solveUsingRecursion(string &text1,string text2,int i,int j){
+    if(i==text1.size()){
+        return 0;
     }
-    int solveUsingMemo(string &text1,string &text2,int i,int j,vector<vector<int>>&dp){
-         if(i==text1.length()){
-            return 0;
-        }
-        if(j==text2.length()){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int ans=0;
-        if(text1[i]==text2[j]){
-            ans=1+solveUsingMemo(text1,text2,i+1,j+1,dp);
-        }
-        else{
-            ans=0+max(solveUsingMemo(text1,text2,i,j+1,dp),solveUsingMemo(text1,text2,i+1,j,dp));
-        }
-        return dp[i][j]=ans;
+    if(j==text2.size()){
+        return 0;
     }
-    int solveUsingTab(string text1,string text2){
-        vector<vector<int>>dp(text1.size()+5,vector<int>(text2.size()+5,0));
-        for(int i=text1.size()-1;i>=0;i--){
-            for(int j=text2.size()-1;j>=0;j--){
-                int ans=INT_MIN;
-             if(text1[i]==text2[j]){
-            ans=1+dp[i+1][j+1];
-            }
-          else{
-               ans=0+max(dp[i][j+1],dp[i+1][j]);
-            }
-            dp[i][j]=ans;
-            }
-        }
-        return dp[0][0];
+    int ans=0;
+    if(text1[i]==text2[j]){
+       ans=1+solveUsingRecursion(text1,text2,i+1,j+1);
     }
-       
-    int solveUsingso(string text1,string text2){
-        //vector<vector<int>>dp(text1.size()+5,vector<int>(text2.size()+5,0));
-        vector<int>curr(text2.size()+1);
-        vector<int>next(text2.size()+1);
-        for(int i=text1.size()-1;i>=0;i--){
-            for(int j=text2.size()-1;j>=0;j--){
-                int ans=INT_MIN;
-             if(text1[i]==text2[j]){
-            ans=1+next[j+1];
-            }
-          else{
-               ans=0+max(curr[j+1],next[j]);
-            }
-            curr[j]=ans;
-            }
-            next=curr;
-        }
-        return next[0];
+    else{
+         ans=0+max(solveUsingRecursion(text1,text2,i+1,j),solveUsingRecursion(text1,text2,i,j+1));
     }
-    
+    return ans;
+ }
+
+ int solveUsingMemo(string &text1,string text2,int i,int j,vector<vector<int>>&dp){
+    if(i==text1.size()){
+        return 0;
+    }
+    if(j==text2.size()){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    int ans=0;
+    if(text1[i]==text2[j]){
+       ans=1+solveUsingMemo(text1,text2,i+1,j+1,dp);
+    }
+    else{
+         ans=0+max(solveUsingMemo(text1,text2,i+1,j,dp),solveUsingMemo(text1,text2,i,j+1,dp));
+    }
+    return dp[i][j]=ans;
+ }
+ 
+
     int longestCommonSubsequence(string text1, string text2) {
-        int i=0;
-        int j=0;
         vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size(),-1));
-        int ans=solveUsingso(text1,text2);
+        int ans=solveUsingMemo(text1,text2,0,0,dp);
         return ans;
     }
 };
