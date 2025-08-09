5f37d959b44cc12b695c75c9427628a07537266d
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-08-09 17:43:22 +0530

    Time: 40 ms (91.98%) | Memory: 29.1 MB (72.13%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>>result;
-    void twosum(vector<int>&nums,int target,int i,int j){
-        while(i<j){
-            if(nums[i]+nums[j]>target){
-                j--;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       vector<vector<int>>result;
+       for(int i=0;i<nums.size();i++){
+        if(i>0 && nums[i]==nums[i-1]){
+            continue;}
+          int num1=nums[i];
+          int j=i+1;
+          int k=nums.size()-1;
+          while(j<k){
+           
+            if(num1+nums[j]+nums[k]==0){
+              while(j<k&& nums[j]==nums[j+1])j++;
+              while(j<k && nums[k]==nums[k-1])k--;
+               result.push_back({num1,nums[j],nums[k]});
+               j++;
+               k--;
+              
             }
-            else if(nums[i]+nums[j]<target){
-                i++;
+            else if(num1+nums[j]+nums[k]<0){
+                j++;
             }
             else{
-                while(i<j && nums[i]==nums[i+1]){
-                    i++;
-                }
-                while(i<j && nums[j]==nums[j-1]){
-                    j--;
-                }
-                result.push_back({-target,nums[i],nums[j]});
-                i++;j--;
+                k--;
             }
-        }
-    }
-    vector<vector<int>> threeSum(vector<int>& nums) {
-
-        //
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-2;i++){
-            if(i>0&&nums[i]==nums[i-1]){
-                continue;
-            }
-            int target=-nums[i];
-            twosum(nums,target,i+1,nums.size()-1);
-
-        }
-        return result;
-        
+          }
+       } 
+       return result;
     }
 };
