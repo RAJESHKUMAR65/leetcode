02ec5b53e8187ea7d1358c109b2080bdc617aaae
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-08-17 16:58:58 +0530

    Time: 36 ms (54.85%) | Memory: 19.2 MB (58.94%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
-int rec(vector<int>&coins,int amount){
-    
-    if(amount<0){
-        return INT_MAX;
-    }
-    if(amount==0){
+// recursive solution
+   int recursionSol_coinchange(vector<int>&coins,int amount){
+      if(amount==0){
         return 0;
-    }
-    int finalans=INT_MAX;
+      }
+      if(amount <0){
+        return INT_MAX;
+      }
+      int final_ans=INT_MAX;
     for(int i=0;i<coins.size();i++){
-         int ans=rec(coins,amount-coins[i]);
-         if(ans!=INT_MAX){
-            finalans=min(finalans,ans+1);
-         }
+      int ans= recursionSol_coinchange(coins,amount-coins[i]);
+       if(ans!=INT_MAX){
+        final_ans=min(ans+1,final_ans);
+       }
     }
-    return finalans;
-}
-int memo(vector<int>&coins,int amount,vector<int>&dp){
-    
-    if(amount<0){
-        return INT_MAX;
-    }
+    return final_ans;
+   }
+
+   int sol_topDown_coinchange(vector<int>&coins,int amount,vector<int>&dp){
     if(amount==0){
         return 0;
-    }
-    if(dp[amount]!=-1){
+      }
+      if(amount <0){
+        return INT_MAX;
+      }
+      if(dp[amount]!=-1){
         return dp[amount];
-    }
-    int finalans=INT_MAX;
+      }
+      int final_ans=INT_MAX;
     for(int i=0;i<coins.size();i++){
-         int ans=memo(coins,amount-coins[i],dp);
-         if(ans!=INT_MAX){
-            finalans=min(finalans,ans+1);
-         }
+      int ans=  sol_topDown_coinchange(coins,amount-coins[i],dp);
+       if(ans!=INT_MAX){
+        final_ans=min(ans+1,final_ans);
+       }
     }
-    return dp[amount]=finalans;
-}
+    return dp[amount]=final_ans;
+   }
     int coinChange(vector<int>& coins, int amount) {
         vector<int>dp(amount+1,-1);
-        int ans=memo(coins,amount,dp);
-        if(ans!=INT_MAX){
-            return ans;
-        }
-        return -1;
+        int ans=sol_topDown_coinchange (coins,amount,dp);
+        if(ans!=INT_MAX) return ans;
+        else return -1;
     }
 };
