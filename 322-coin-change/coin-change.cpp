960a51793b55444c3f76e04d4c46b57d2ee02659
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-08-20 00:06:48 +0530

    Time: 27 ms (66.82%) | Memory: 22.1 MB (52.48%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -37,9 +37,26 @@ public:
     }
     return dp[amount]=final_ans;
    }
+   int sol_bottomDown_coinchange(vector<int>&coins,int amount){
+    vector<int>dp(amount+1,INT_MAX);
+    dp[0]=0;
+    for(int target=1;target<=amount;target++){
+        for(int i=0;i<coins.size();i++){
+            if(target-coins[i]>=0 && target-coins[i]!=INT_MAX){
+                 int ans=dp[target-coins[i]];
+                 if(ans!=INT_MAX){
+                 dp[target]=min(ans+1,dp[target]);}
+            }
+           
+        }
+    }
+    return dp[amount];
+   }
     int coinChange(vector<int>& coins, int amount) {
         vector<int>dp(amount+1,-1);
-        int ans=sol_topDown_coinchange (coins,amount,dp);
+
+        
+        int ans=sol_bottomDown_coinchange(coins,amount);
         if(ans!=INT_MAX) return ans;
         else return -1;
     }
