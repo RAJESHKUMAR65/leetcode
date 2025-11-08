1f219921d2cbeb0c9ec8938afeee6c74c87fa9aa
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-11-08 13:27:25 +0530

    Time: 181 ms (17.77%) | Memory: 111.3 MB (19.30%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
-    bool checksubarray(vector<int>&nums,int i,int sum,vector<vector<int>>&dp){
-        if(sum==0){
-            return true;
-        }
-        if(sum<0){
-            return false;
-        }
-        if(i>=nums.size()){
-            return false;
-        }
-        if(dp[i][sum]!=-1){
-            return dp[i][sum];
-        }
-        int include=checksubarray(nums,i+1,sum-nums[i],dp);
-        int exclude=checksubarray(nums,i+1,sum,dp);
-        return dp[i][sum]=(include || exclude);
+bool solveUsingRecursion(int i,vector<int>&nums,int target){
+   
+    if(i>=nums.size()){
+        return false;
     }
+    if(target==0)return true;
+    if(target<0)return false;
+    int include=solveUsingRecursion(i+1,nums,target-nums[i]);
+    int exclude=solveUsingRecursion(i+1,nums,target);
+    return (include||exclude);
+    
+}
+bool solveUsingMemo(int i,vector<int>&nums,int target,vector<vector<int>>&dp){
+   
+    if(i>=nums.size()){
+        return false;
+    }
+    if(target==0)return true;
+    if(target<0)return false;
+    if(dp[i][target]!=-1){
+        return dp[i][target];
+    }
+    int include=solveUsingMemo(i+1,nums,target-nums[i],dp);
+    int exclude=solveUsingMemo(i+1,nums,target,dp);
+    dp[i][target]= include||exclude;
+    return dp[i][target];
+    
+}
     bool canPartition(vector<int>& nums) {
         int sum=0;
-        ;
-       for(int i=0;i<nums.size();i++){
-        sum+=nums[i];
-       } 
-       if(sum&1) return false;
-       sum=sum/2;
-       int i=0;
-       vector<vector<int>>dp(nums.size()+1,vector<int>(sum+1,-1));
-       bool ans=checksubarray(nums,i,sum,dp);
-       return ans;
+        for(int i=0;i<nums.size();i++){
+           sum+=nums[i];
+        }
+        if(sum&1){
+            return false;
+        }
+        // bool ans=solveUsingRecursion(0,nums,sum/2);
+        // return ans;
+
+        // memo
+        int target=sum/2;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,-1));
+      bool ans=solveUsingMemo(0,nums,target,dp);
+      return ans;
     }
 };
