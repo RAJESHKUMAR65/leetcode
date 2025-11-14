dae34c6a008f714d6d013149dd6da36f376c6747
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-11-14 17:20:46 +0530

    Time: 83 ms (33.00%) | Memory: 11.1 MB (53.82%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,59 +1,38 @@
 class Solution {
 public:
-   int solveUsingRecursion(int s,int e){
-    if(s>=e){
-         return 0;
-    }
+  int solveUsingRecursion(int s,int e){
 
+    if(s>=e){
+      return 0;
+    }
     int ans=INT_MAX;
     for(int i=s;i<=e;i++){
         ans=min(ans,i+max(solveUsingRecursion(s,i-1),solveUsingRecursion(i+1,e)));
     }
     return ans;
-   }
-   int solveUsingMemo(int s,int e,vector<vector<int>>&dp){
-        if(s>=e){
-         return 0;
-     }
 
-    int ans=INT_MAX;
+  }
+   int solveUsingMemo(int s,int e,vector<vector<int>>&dp){
+
+    if(s>=e){
+      return 0;
+    }
     if(dp[s][e]!=-1){
         return dp[s][e];
     }
+    int ans=INT_MAX;
     for(int i=s;i<=e;i++){
         ans=min(ans,i+max(solveUsingMemo(s,i-1,dp),solveUsingMemo(i+1,e,dp)));
     }
-    return dp[s][e]= ans;
-   }
-   int solveUsingTab(int n){
-    vector<vector<int>>dp(n+3,vector<int>(n+4,0));
-    for(int s=n;s>=1;s--){
-        for(int e=1;e<=n;e++){
-            int ans=INT_MAX;
-            if(s>=e){
-                continue;
-            }
-            else{
-                  for(int i=s;i<=e;i++){
-            
-                     ans=min(ans,i+max(dp[s][i-1],dp[i+1][e]));
-                }
+    dp[s][e]= ans;
+    return dp[s][e];
 
-            }
-          
-             
-              
-              dp[s][e]= ans;
-        }
-
-        }
-        return dp[1][n];
-    }
-   
+  }
     int getMoneyAmount(int n) {
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        
-        int ans=solveUsingTab(n);
-        return ans;
+       //int ans=solveUsingRecursion(1,n); 
+       vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+       int ans=solveUsingMemo(1,n,dp);
+       
+       return ans;
     }
 };
