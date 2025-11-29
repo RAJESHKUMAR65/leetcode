f798fdf7c95b748f897d270b8d905b1d1094fc5b
RAJESHKUMAR65 <110731463+RAJESHKUMAR65@users.noreply.github.com>
2025-11-29 14:05:52 +0530

    Time: 79 ms (52.91%) | Memory: 76.3 MB (7.77%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
-   int solveUsingRecursion(string &s,int i,int j){
-     if(i>j){
-        return 0;
-     }
-     int ans=0;
-     if(s[i]==s[j]){
-       ans=2+solveUsingRecursion(s,i+1,j-1);
-     }
-     else{
-        ans=0+max(solveUsingRecursion(s,i+1,j),solveUsingRecursion(s,i,j-1));
-     }
-     return ans;
-   }
-   int solveUsingMemo(string &s,int i,int j,vector<vector<int>>&dp){
-     if(i>j){
-        return 0;
-     }
-     if(i==j){
-        return 1;
-     }
-     int ans=0;
-     if(dp[i][j]!=-1){
-        return dp[i][j];
-     }
-     if(s[i]==s[j]){
-       ans=2+solveUsingMemo(s,i+1,j-1,dp);
-     }
-     else{
-        ans=0+max(solveUsingMemo(s,i+1,j,dp),solveUsingMemo(s,i,j-1,dp));
-     }
-     return dp[i][j]=ans;
+    int solveUsingRecursion(string &s1,string &s2,int i,int j){
+       if(i>=s1.length()) return 0;
+       if(j>=s2.length()) return 0;
+       int len=0;
+       if(s1[i]==s2[j]){
+          len=1+solveUsingRecursion(s1,s2,i+1,j+1);
+       }else{
+         len=0+max(solveUsingRecursion(s1,s2,i+1,j),solveUsingRecursion(s1,s2,i,j+1));
+       }
+       return len;
+    }
 
-   }
+    int solveUsingMemo(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
+        if(i>=s1.length()) return 0;
+        if(j>=s2.length()) return 0;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int len=0;
+        if(s1[i]==s2[j]){
+            len=1+solveUsingMemo(s1,s2,i+1,j+1,dp);
+        }else{
+            len=0+max(solveUsingMemo(s1,s2,i+1,j,dp),solveUsingMemo(s1,s2,i,j+1,dp));
+        }
+        dp[i][j]=len;
+        return dp[i][j];
+    }
+
     int longestPalindromeSubseq(string s) {
-        int i=0;
-        int j=s.length()-1;
+       string s1=s;
+       string s2=s;
+       reverse(s2.begin(),s2.end());
+       int i=0;
+       int j=0;
+      // int ans=solveUsingRecursion(s1,s2,i,j);
+      vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
+    int ans=solveUsingMemo(s1,s2,i,j,dp);
+       return ans;
         
-        int n=s.length();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans=solveUsingMemo(s,i,j,dp);
-        return ans;
     }
 };
